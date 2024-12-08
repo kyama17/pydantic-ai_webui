f2837f09f65f3cfb5d3f6c12509e66dfbad34d00
kyama17 <97741094+kyama17@users.noreply.github.com>
2024-12-08 19:41:00 +0900

    Change placeholder

--- bank_support.py
+++ bank_support.py
@@ -88,7 +88,7 @@ for message in st.session_state.messages:
 
 # Create a chat input field to allow the user to enter a message. This will display
 # automatically at the bottom of the page.
-if prompt := st.chat_input("Where does 'hello world' come from?"):
+if prompt := st.chat_input("What is my balance?"):
     
     # Store and display the current prompt.
     st.session_state.messages.append({"role": "user", "content": prompt})
