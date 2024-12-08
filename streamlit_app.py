4aacef73f14d0399c5ad2aff2c50979a6f96cc0d
kyama17 <97741094+kyama17@users.noreply.github.com>
2024-12-08 19:36:50 +0900

    Add bank support

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
-pg = st.navigation([
-    st.Page("hello_world.py"),
-    ])
+hello_world = st.Page("hello_world.py", title="Hello World Example", icon=":material/waving_hand:")
+bank_support = st.Page("bank_support.py", title="Bank support", icon=":material/account_balance:")
+
+pg = st.navigation([hello_world, bank_support])
+
 st.set_page_config(page_title="Pydantic WebUI", page_icon=":robot_face:")
 pg.run()
